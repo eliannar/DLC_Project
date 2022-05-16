0fd82788464283a2a7408d736b1af26facd61707
elianna <eliannar@gmail.com>
2022-05-16 12:42:41 +0300

    Added trial_times index, added helper functions, and restructured some of the code for clarity.

--- Kinematic_Analysis/Day.py
+++ Kinematic_Analysis/Day.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from mat4py import loadmat
 
+# TODO format date
 DATA_PATH = r'Z:\elianna.rosenschein\n270122\DLC_Analysis\nana-trial{0}_DLC_3D_SIDE.csv '
 ED_PATH = r'Z:\elianna.rosenschein\n270122\EDfiles\n{0}ee.{1}.mat'
 INFO_FILE = r'Z:\elianna.rosenschein\n270122\Info\n270122_param.mat'
@@ -10,28 +11,37 @@ INDEX_FILE = r'Z:\elianna.rosenschein\alignment_indices_n270122.mat'
 VIDEO_INFO_FILE = r'Z:\elianna.rosenschein\vidInfo_n270122'
 
 # dictionary that links the body parts from the analysis to the colomns names
-BODY_PART = 'finger'  #body part that we want to analyze
+BODY_PART = 'finger'  # body part that we want to analyze
 body_cols_side = {'finger': ['DLC_3D_SIDE', 'DLC_3D_SIDE.1', 'DLC_3D_SIDE.2'],
-                      'wrist': ['DLC_3D_SIDE.3', 'DLC_3D_SIDE.4', 'DLC_3D_SIDE.5'],
-                      'elbow': ['DLC_3D_SIDE.6', 'DLC_3D_SIDE.7', 'DLC_3D_SIDE.8']}
+                  'wrist': ['DLC_3D_SIDE.3', 'DLC_3D_SIDE.4', 'DLC_3D_SIDE.5'],
+                  'elbow': ['DLC_3D_SIDE.6', 'DLC_3D_SIDE.7', 'DLC_3D_SIDE.8']}
 body_cols_top = {'finger': ['DLC_3D_TOP', 'DLC_3D_TOP.1', 'DLC_3D_TOP.2'],
                  'wrist': ['DLC_3D_TOP.3', 'DLC_3D_TOP.4', 'DLC_3D_TOP.5'],
                  'elbow': ['DLC_3D_TOP.6', 'DLC_3D_TOP.7', 'DLC_3D_TOP.8']}
 
+camera_angle = {'SIDE': body_cols_side, 'TOP': body_cols_top}
 
-prep_angle = {'SIDE': body_cols_side, 'TOP': body_cols_top}
 
 class Day:
     id = 0
     num_of_subsessions = 0
-    # I need a table with day, subsession,
-    subsess_data = {}
+    subsess_files = None
+    hfs_subsess = None
+    burst_subsess = None
+    csv_indices = None
+    trial_data = None
 
     def __init__(self):
-        self.trial_data = pd.DataFrame({'TrialNum': [], 'csvNum': [], 'valid': [], 'subSess': [], 'target': [], 'update': [], 'HFS': [], 'Burst': []})
-
+        self.trial_data = pd.DataFrame(
+            {'TrialNum': [], 'csvNum': [], 'valid': [], 'subSess': [], 'target': [], 'update': [], 'HFS': [],
+             'Burst': [], 'TrialTimes': []})
 
     def load_info_file(self, info_path):
+        """
+        Loads info from Info File for this day
+        :param info_path:
+        :return:
+        """
         info_file = loadmat(info_path)
         self.id = info_file['DDFparam']['ID']
         self.num_of_subsessions = len(info_file['SESSparam']['SubSess']['Files'])
@@ -40,37 +50,63 @@ class Day:
         self.burst_subsess = info_file['SESSparam']['fileConfig']['BURST']
         index_file = loadmat(INDEX_FILE)
         self.csv_indices = index_file
-        self.load_ed_files()
 
     def load_ed_files(self):
-        running_count = 0
-        for i in range(self.num_of_subsessions):
-            self.subsess_data[i] = []
-            files_start = self.subsess_files[i][0]
-            files_end = self.subsess_files[i][1]
-            for j in range(files_end + 1 - files_start):
-                path = ED_PATH.format(str(self.id) + '0' + str(i + 1), j + 1)
+        """
+        Fills Dataframe with each row representing a trial, containing all the relevant information from the ED files
+        :return:
+        """
+        running_count = 0  # counter for ALL trials from day
+        for subsess in range(self.num_of_subsessions):
+            files_start, files_end = self.subsess_files[subsess] #todo check unpacking
+            for subsess_file in range(files_end + 1 - files_start):  # subsess_file is file index in files from ONE subsession
+                path = ED_PATH.format(str(self.id) + '0' + str(subsess + 1), subsess_file + 1)
                 ed_file = loadmat(path)
-                for t in range(len(ed_file['trials'])):
+                invalid_counter = 0
+                for trial in range(len(ed_file['trials'])):  # trial is trial index from one file from one subsession
                     running_count += 1
-                    if [running_count] in self.csv_indices['I']:
-                        general_ind = self.csv_indices['I'].index([running_count])
-                        csv_ind = self.csv_indices['J'][general_ind][0]
-                    else:
-                        csv_ind = None
-                    j_offset = self.subsess_files[i][0] + j - 1
-                    #TODO different kinds of burst: 25 = low (assign 2), empty/130 = high (assign 1), from electrode.1.stim.amp
-                    #TODO trial times
-                    temp = {'TrialNum': running_count, 'csvNum': csv_ind, 'subSess': i + 1, 'valid': ed_file['trials'][t][2], 'target': ed_file['trials'][t][4], 'update': ed_file['trials'][t][5], 'HFS': self.hfs_subsess[j_offset], 'Burst': self.burst_subsess[j_offset]} #TODO change j index
+                    file_offset = self.subsess_files[subsess][0] + subsess_file - 1  # file index in all files from day
+                    csv_ind = self.find_csv_index(running_count)
+                    invalid_counter, trial_times_ind = self.find_trialtimes_index(ed_file, invalid_counter, trial)
+                    # TODO different kinds of burst: 25 = low (assign 2), empty/130 = high (assign 1), from electrode.1.stim.amp
+                    temp = {'TrialNum': running_count, 'csvNum': csv_ind, 'subSess': subsess + 1,
+                            'valid': ed_file['trials'][trial][2], 'target': ed_file['trials'][trial][4],
+                            'update': ed_file['trials'][trial][5], 'HFS': self.hfs_subsess[file_offset],
+                            'Burst': self.burst_subsess[file_offset],
+                            'TrialTimes': ed_file['TrialTimes'][trial_times_ind]}  # TODO check trialtimes access
                     self.trial_data = self.trial_data.append(temp, ignore_index=True)
 
+    def find_csv_index(self, running_count):
+        if [running_count] in self.csv_indices['I']:
+            general_ind = self.csv_indices['I'].index([running_count])
+            return self.csv_indices['J'][general_ind][0]
+        else:
+            return None
 
-    def process_data(data_path, angle):
-        data = pd.read_csv(data_path, header=0, usecols=prep_angle[angle][BODY_PART])
+    def find_trialtimes_index(self, edFile, invalid_trial_counter, trial_index):
+        """
+        returns an updated invalid_trial_counter (adds 1 if this trial is invalid), and returns a shifted counter if
+        for TrialTimes (if this trial is valid)
+        :param edFile:
+        :param invalid_trial_counter:
+        :param trial_index:
+        :return:
+        """
+        ret1 = invalid_trial_counter
+        ret2 = None
+        if not (edFile['trials'][trial_index][2]):
+            ret1 += 1
+        if edFile['trials'][trial_index][2]:
+            ret2 = trial_index - invalid_trial_counter
+        return ret1, ret2
+
+
+    def process_data(self, data_path, angle):
+        data = pd.read_csv(data_path, header=0, usecols=camera_angle[angle][BODY_PART])
         # rename headers:
         headers_names = ['{0}_x'.format(BODY_PART), '{0}_y'.format(BODY_PART), '{0}_z'.format(BODY_PART)]
-        body_part_cols = [prep_angle[angle][BODY_PART][0], prep_angle[angle][BODY_PART][1],
-                          prep_angle[angle][BODY_PART][2]]
+        body_part_cols = [camera_angle[angle][BODY_PART][0], camera_angle[angle][BODY_PART][1],
+                          camera_angle[angle][BODY_PART][2]]
         d = dict(zip(body_part_cols, headers_names))
         partial_data = data.rename(columns=d, inplace=False)
         partial_data = partial_data.drop([0, 1])
@@ -84,7 +120,12 @@ class Day:
                                                                 downcast="float")
         return partial_data
 
+    def preprocess(self):
+        self.load_info_file(INFO_FILE)
+        self.load_ed_files()
+        self.process_data(DATA_PATH, 'SIDE')
 
 
-day = Day()
-day.load_info_file(INFO_FILE)
+if __name__ == "__main__":
+    day = Day()
+    day.preprocess()
